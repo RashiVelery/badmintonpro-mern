acf01e6d1fcc65bb86e462b4a4da955356b0a451
RashidVelery <rashivelery@gamil.com>
2026-03-11 16:40:24 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,9 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173"  ],
+    origin: ["http://localhost:5173",
+        "https://badmintonpro-mernfrontend.vercel.app"
+    ],
     credentials: true
 }))
 
